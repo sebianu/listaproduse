a85ccdfe1f9fed949aae7b6b765d2fe991cb532b
Mircea <mirceamunteanu232@gmail.com>
2022-11-15 19:15:55 +0200

    Adaugat clasa de bon

--- src/mydomain/Bon.java
+++ src/mydomain/Bon.java
@@ -1,6 +1,43 @@
 package mydomain;
 
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime;    
+import java.util.HashMap;
+import java.util.Map;
+
+import mydomain.ReadList.map;
+
 public class Bon {
-    //hashmap
+    private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"); 
+    private HashMap<Integer, String> listaProduse = new HashMap<Integer, String>();
+    
+    public Bon() {
+    	
+    }
+    
+    public String printBon() { 
+    	LocalDateTime timpAcum = LocalDateTime.now();
+    	String bonPrintOut = "        BON FISCAL        \n";
+    	for(Map.Entry<Integer, String> entry : listaProduse.entrySet()) {
+    		Integer prodCant = entry.getKey();
+    		String prodNume = entry.getValue();
+    		bonPrintOut += String.format("%s - %d\n", prodNume, prodCant);
+    	}
+    	bonPrintOut += "\n " + dtf.format(timpAcum) + "\n";
+    	return bonPrintOut;
+    }
+    
+    public void addProdus(int produs) {
+    	if(listaProduse.containsKey(produs)) {
+    		listaProduse.put(produs, listaProduse.get(produs) + 1);
+    	}
+    	listaProduse.put(map.get(produs), 1);
+    }
+    
+    public void delProdus(int produs) {
+    	listaProduse.remove(produs);
+    }
+    
+    
 
 }
